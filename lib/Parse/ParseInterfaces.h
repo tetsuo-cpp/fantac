de57a9cd87a6bb603ac306c5c6428331b938e837
Alex Cameron <ascottcameron@gmail.com>
2018-06-02 15:10:15 +1000

    Added some more types for lexing.

--- lib/Parse/ParseInterfaces.h
+++ lib/Parse/ParseInterfaces.h
@@ -1,9 +1,12 @@
 #pragma once
 
+#include <stdexcept>
 #include <string>
+#include <vector>
 
 namespace fantac::parse {
 
+// Generic exception for parsing related errors.
 class ParseException : public std::runtime_error {
 public:
   ParseException(const std::string &Error);
@@ -12,5 +15,26 @@ public:
 
 ParseException::ParseException(const std::string &Error)
     : std::runtime_error(Error) {}
+
+enum TokenKind { TK_String, TK_Eof };
+
+struct Token {
+  template <typename String>
+  Token(TokenKind Kind, String &&Id)
+      : Kind(Kind), Id(std::forward<String>(Id)) {}
+  Token(TokenKind Kind) : Kind(Kind) {}
+
+  TokenKind Kind;
+  const std::string Id;
+};
+
+class ITokenLexer {
+public:
+  virtual ~ITokenLexer() = default;
+
+  // TODO: Might be good to provide functions for getting the current token and
+  // peeking forward N tokens, etc. Just need to figure out what I need.
+  virtual const std::vector<Token> &getTokens() const = 0;
+};
 
 } // namespace fantac::parse
--- lib/Parse/TokenLexer.h
+++ lib/Parse/TokenLexer.h
@@ -1,20 +1,30 @@
 #pragma once
 
+#include "ParseInterfaces.h"
+
 namespace fantac::parse {
 
-class TokenLexer {
+class TokenLexer : public ITokenLexer {
 public:
   virtual ~TokenLexer() = default;
 
   template <typename Iterator> void read(Iterator Begin, Iterator End);
 
+  // ITokenLexer impl.
+  virtual const std::vector<Token> &getTokens() const override;
+
 private:
+  std::vector<Token> Tokens;
 };
 
 template <typename Iterator>
 void TokenLexer::read(Iterator Begin, Iterator End) {
-  static_cast<void>(Begin);
-  static_cast<void>(End);
+  while (Begin != End) {
+  }
+
+  Tokens.emplace_back(TK_Eof);
 }
+
+const std::vector<Token> &TokenLexer::getTokens() const { return Tokens; }
 
 } // namespace fantac::parse
