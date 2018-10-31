fedcd901cfa1d9fe5bd2e344bc838b15368f2559
Alex Cameron <ascottcameron@gmail.com>
2018-10-31 23:18:51 +1100

    Add new token types to string conversion function.

--- lib/Parse/ParseInterfaces.h
+++ lib/Parse/ParseInterfaces.h
@@ -97,6 +97,8 @@ inline std::string tokenKindToString(TokenKind Kind) {
     return "Else";
   case TokenKind::TK_For:
     return "For";
+  case TokenKind::TK_While:
+    return "While";
   case TokenKind::TK_Return:
     return "Return";
   case TokenKind::TK_OpenBrace:
@@ -115,18 +117,70 @@ inline std::string tokenKindToString(TokenKind Kind) {
     return "Colon";
   case TokenKind::TK_Add:
     return "Add";
+  case TokenKind::TK_AddEq:
+    return "AddEq";
   case TokenKind::TK_Subtract:
     return "Subtract";
+  case TokenKind::TK_SubtractEq:
+    return "SubtractEq";
   case TokenKind::TK_Multiply:
     return "Multiply";
+  case TokenKind::TK_MultiplyEq:
+    return "MultiplyEq";
   case TokenKind::TK_Divide:
     return "Divide";
+  case TokenKind::TK_DivideEq:
+    return "DivideEq";
+  case TokenKind::TK_Modulus:
+    return "Modulus";
+  case TokenKind::TK_ModulusEq:
+    return "ModulusEq";
+  case TokenKind::TK_Assign:
+    return "Assign";
   case TokenKind::TK_LessThan:
     return "LessThan";
+  case TokenKind::TK_LessThanEq:
+    return "LessThanEq";
   case TokenKind::TK_GreaterThan:
     return "GreaterThan";
+  case TokenKind::TK_GreaterThanEq:
+    return "GreaterThanEq";
   case TokenKind::TK_Equals:
     return "Equals";
+  case TokenKind::TK_NotEquals:
+    return "NotEquals";
+  case TokenKind::TK_ShiftLeft:
+    return "ShiftLeft";
+  case TokenKind::TK_ShiftLeftEq:
+    return "ShiftLeftEq";
+  case TokenKind::TK_ShiftRight:
+    return "ShiftRight";
+  case TokenKind::TK_ShiftRightEq:
+    return "ShiftRightEq";
+  case TokenKind::TK_And:
+    return "And";
+  case TokenKind::TK_AndEq:
+    return "AndEq";
+  case TokenKind::TK_Or:
+    return "Or";
+  case TokenKind::TK_OrEq:
+    return "OrEq";
+  case TokenKind::TK_Xor:
+    return "Xor";
+  case TokenKind::TK_XorEq:
+    return "XorEq";
+  case TokenKind::TK_LogicalAnd:
+    return "LogicalAnd";
+  case TokenKind::TK_LogicalOr:
+    return "LogicalOr";
+  case TokenKind::TK_Period:
+    return "Period";
+  case TokenKind::TK_Arrow:
+    return "Arrow";
+  case TokenKind::TK_Increment:
+    return "Increment";
+  case TokenKind::TK_Decrement:
+    return "Decrement";
   case TokenKind::TK_EOF:
     return "EOF";
   case TokenKind::TK_None:
